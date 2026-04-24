9724dc60c3eb61a9e2fa9a20ff678ef2b7aed96b
Yingyingcheng <doris82717@gmail.com>
2026-04-23 18:52:38 -0700

    Basic TicTacToe

--- practice/react-practice/src/components/TicTacToe.jsx
+++ practice/react-practice/src/components/TicTacToe.jsx
@@ -1,39 +1,134 @@
 import "../App.css";
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-  const handleCliclk = () => {
-    setValue("X");
-  };
-
+// Square component receive the value prop from the TicTacToe component.
+function Square({ value, onSquareClick }) {
   return (
-    <>
-      <button className="square" onClick={handleCliclk}>
-        {value}
-      </button>
-    </>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
 export default function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    //.slice() to create a copy of the squares array
+    const nextSquares = squares.slice();
+    //check if the square is already filled with "X" or "O"
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext); // omggggg!!!
+  };
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null; // why ????
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner:" + winner;
+  } else {
+    status = "Player:" + (xIsNext ? "X" : "O");
+  }
   return (
     <>
       <h2 style={{ backgroundColor: "pink", padding: "5px" }}>SECTION 3</h2>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        {/* () => handleClick(i) is an arrow function */}
+        {/* When the square is clicked, 
+        the code after the => arrow will run, 
+        calling handleClick(i). */}
+        <Square
+          value={squares[0]}
+          onSquareClick={() => {
+            handleClick(0);
+          }}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => {
+            handleClick(1);
+          }}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => {
+            handleClick(2);
+          }}
+        />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          value={squares[3]}
+          onSquareClick={() => {
+            handleClick(3);
+          }}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => {
+            handleClick(4);
+          }}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => {
+            handleClick(5);
+          }}
+        />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square
+          value={squares[6]}
+          onSquareClick={() => {
+            handleClick(6);
+          }}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => {
+            handleClick(7);
+          }}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => {
+            handleClick(8);
+          }}
+        />
       </div>
     </>
   );
